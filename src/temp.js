6e4d2d4ff01dc113736c7667225250d46a570b63
Muhammad-Jawwad <muhammadjawwad417@gmail.com>
2024-06-03 19:37:45 +0500

    change in form size

--- src/temp.js
+++ src/temp.js
@@ -1,5 +1,2 @@
 export const apiUrl = "https://nearest-pharma-be.vercel.app";
 // export const apiUrl = "http://localhost:8080"
-
-export const googleAPIKey = "AIzaSyB-xrs1XwqgisBgKXFSJn_x99NoxDuzNkk"
-// export const googleAPIKey = "AIzaSyDtTeKzLdvEensF5z-LUodFfRDTv-WMnkA"
